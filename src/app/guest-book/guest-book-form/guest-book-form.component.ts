bc78608f62050dc1e665bbc2611b17efd796347a
Artjoms Voroncovs (external) <Artjoms_Voroncovs@swissre.com>
2022-01-21 16:29:22 +0200

    feat(feature/guest-book): Added validators on required fields, Added error messages support for form

--- src/app/guest-book/guest-book-form/guest-book-form.component.ts
+++ src/app/guest-book/guest-book-form/guest-book-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { createGuestInvite } from '../state/actions/guest-book-page.actions';
 
@@ -8,19 +8,43 @@ import { createGuestInvite } from '../state/actions/guest-book-page.actions';
     templateUrl: './guest-book-form.component.html',
     styleUrls: ['./guest-book-form.component.scss']
 })
-export class GuestBookFormComponent  {
+export class GuestBookFormComponent {
+
+    name: FormControl = new FormControl('');
+    surname: FormControl = new FormControl('');
+    email: FormControl = new FormControl('');
+    message: FormControl = new FormControl('', [
+        Validators.required, Validators.minLength(20)
+    ]);
+    author: FormControl = new FormControl('', [
+        Validators.required
+    ]);
 
     guestBookForm = new FormGroup({
-        name: new FormControl(''),
-        surname: new FormControl(''),
-        email: new FormControl(''),
-        message: new FormControl(''),
-        author: new FormControl(''),
+        name: this.name,
+        surname: this.surname,
+        email: this.email,
+        message: this.message,
+        author: this.author
     })
 
     constructor(private store: Store) { }
 
-    onSubmit(){
-        this.store.dispatch(createGuestInvite({guestInvite: this.guestBookForm.value}));
+    onSubmit() {
+        this.guestBookForm.valid && this.store.dispatch(createGuestInvite({ guestInvite: this.guestBookForm.value }));
+    }
+
+    getErrorMessage(field: FormControl): string {
+
+        console.log(field)
+        if (field.hasError('required')) {
+            return 'You must enter the value.'
+        }
+
+        if (field.hasError('minlength')) {
+            return `Min length is ${field.errors!['minlength'].requiredLength} letters.`
+        }
+
+        return 'Field is invalid.'
     }
 };
